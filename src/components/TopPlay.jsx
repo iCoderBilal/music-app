5a60c219b1cca122a68d3eb02a944926d3a9af5e
Bilal-12-2 <91077424+iCoderBilal@users.noreply.github.com>
2022-12-30 04:49:24 +0500

    TopPlay

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -12,9 +12,9 @@ import "swiper/css/free-mode";
 
 const TopPlay = () => {
  const dispatch = useDispatch();
- const {activeSong, isPlaying} = useSelector((state)=> state.player)
- const data = useGetTopChartsQuery()
- const divRef = useRef(null)
+ const {activeSong, isPlaying} = useSelector((state)=> state.player);
+ const data = useGetTopChartsQuery();
+ const divRef = useRef(null);
 //  const topPlays = data?.slice(0, 5);
 
 
@@ -31,8 +31,23 @@ useEffect(()=>{
 });
 
 return (
-  <div ref={divRef}>
-    
+  <div ref={divRef} className="xl:ml-6 ml-0 xl:mb-0 mb-6 flex-1 xl:max-w-[500px] max-full-w flex flex-col">
+   <div className="w-full flex flex-col">
+    <div className="flex flex-row justify-between items-center">
+      <h2 className="text-white font-bold text-2xl">
+        Top Charts
+      </h2>
+     <Link to='/top-charts'>
+     <p className="text-gray-300 text-base cursor-pointer" >See More</p>
+     </Link>
+    </div>
+
+    <div className="mt-4 flex flex-col gap-1">
+     {
+      
+     }
+    </div>
+   </div>
   </div>
 )
 
