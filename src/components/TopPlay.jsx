c9b7008bf58e594dd87fd27fb5f5fa1e12885fcd
Bilal-12-2 <91077424+iCoderBilal@users.noreply.github.com>
2023-01-03 08:31:11 +0500

    songDetails finished

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -22,7 +22,7 @@ const TopChartCard = ({song, i, activeSong, isPlaying, handlePauseClick, handleP
    <div className="flex flex-1 flex-col mx-3 justify-center">
    <Link to={`/songs/${song?.key}`}>
     <p className="text-xl font-bold text-white">{song?.title}</p>
-   
+  
    </Link>
    <Link to={`/artists/${song?.artists[0].adamid}`}>
     <p className="text-base text-gray-300 mt-1">{song?.subtitle}</p>
--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -12,15 +12,21 @@ const SongDetails = () => {
     const { songid } = useParams();
     const { activeSong, isPlaying } = useSelector((state) => state.player)
     const { data: songData, isFetching: isFetchingSongDetails } = useGetSongDetailsQuery({ songid });
-    console.log(songData)
+
+    console.log(songData?.images?.coverart)
+   
 
     return(
         <div className="flex flex-col">
+               <DetailsHeader
+        artistId=""
+        songData={songData}
+      />
                  <div className="mb-10">
         <h2 className="text-white text-3xl font-bold">Lyrics:</h2>
         <div className="mt-5">
 
-            {songData?.sections[1].type == "LYRICS" ? songData?.sections[1].text.map((line,i)=>(<p>{line}</p>)) : <p>Sorry, NO lyrics Found!</p>}
+            {songData?.sections[1].type == "LYRICS" ? songData?.sections[1].text.map((line,i)=>(<p className="text-gray-400 text-base my-1">{line}</p>)) : <p className="text-gray-400 text-base my-1">Sorry, NO lyrics Found!</p>}
 
         </div>
         </div>
